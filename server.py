b9d6bbfec985aa514c4a22010926a30a09a87414
Rudik Poghosyan <rudpoghosyan7@gmail.com>
2025-08-21 13:02:50 +0400

    classServer

--- server.py
+++ server.py
@@ -1,1 +1,18 @@
-print("server")
+class Server:
+    def __init__(self, name, ip):
+        self.name = name
+        self.ip = ip
+        self.status = "stopped"
+
+    def start(self):
+        status = "running"
+        print(f"Server {self.name} started")
+
+    def stop(self):
+        status = "stopped"
+        print(f"Server {self.name} stopped")
+
+    def status_info(self):
+        print(f"Server {self.name} is {self.status}") 
+
+
