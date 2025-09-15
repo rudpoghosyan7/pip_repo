3e9878078d2f162e6d3aa4fdbf1472f8bc502093
Rudik <rudpoghosyan7@gmail.com>
2025-09-15 17:11:23 +0400

    commands OS

--- system_os.py
+++ system_os.py
@@ -1,1 +1,23 @@
-print("Hello")
+import os
+
+# Show current directory
+print("Current dir:", os.getcwd())
+
+# List files
+print("Files here:", os.listdir("."))
+
+# Make a new folder
+os.mkdir("test_folder")
+print("Created folder test_folder")
+
+# Change directory
+os.chdir("test_folder")
+print("Now inside:", os.getcwd())
+
+# Go back
+os.chdir("..")
+print("Back to:", os.getcwd())
+
+# Remove folder
+os.rmdir("test_folder")
+print("Removed folder test_folder")
